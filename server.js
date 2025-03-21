538d26a3ae34eaf69e186dfa87cdf692a0c9ffba
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2025-03-20 19:28:09 -0700

    Setup Files for Practice

---
+++ server.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.listen(PORT, console.log(`Server running on http://localhost:${PORT}`));
